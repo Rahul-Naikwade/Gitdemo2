433a5cad4d601ba7803dc830c960b0deb28cfbb7
Rahul-Naikwade <rahulnaiwade94@gmail.com>
2019-12-17 15:21:14 +0530

    Adddd New Data

---
+++ PaymentGateWayExample-1/src/main/java/com/wcs/paymentgate1/PaymentGateWayExample1Application.java
@@ -0,0 +1,13 @@
+package com.wcs.paymentgate1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGateWayExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGateWayExample1Application.class, args);
+	}
+
+}
